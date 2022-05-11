090271dbd99480a59e56794c4e363431b2febc45
tunayildizuzh <erdemtuna.yildiz@uzh.ch>
2022-05-11 13:51:56 +0200

    Predator Implemented.

--- Final Project/BoidsProject/boid.py
+++ Final Project/BoidsProject/boid.py
@@ -1,5 +1,5 @@
 import numpy as np
-from p5 import setup, draw, size, background,run,Vector,stroke,triangle,color,fill
+from p5 import setup, draw, size, background,run,Vector,stroke,triangle,color,fill,circle
 
 class Boid():
 
@@ -22,7 +22,7 @@ class Boid():
         tri_len = (np.sqrt(3) /3) * side
         tri_len2 = (np.sqrt(3) /6) * side
 
-        fill(255,204,0)
+        fill('white')
         A = (self.position[0],self.position[1]+tri_len)
         B = (self.position[0] - (side/2),self.position[1]- tri_len2)
         C = (self.position[0] + (side/2),self.position[1]- tri_len2)
@@ -49,3 +49,54 @@ class Boid():
             self.position.y = 0
         elif self.position.y < 0:
             self.position.y = self.height
+
+    def alignment(self,boids): # Boids tends to TURN in same direction with nearby boids.
+        steering_angle = Vector([0,0])
+        pack_angle = Vector([0,0])
+        no_of_boids = 0
+
+        for boid in boids:
+            if np.linalg.norm(boid.position - self.position) < 50: # Defining the area of flocking.
+                pack_angle += boid.velocity
+                no_of_boids +=1
+
+        if no_of_boids > 1:
+            pack_angle = pack_angle / np.linalg.norm(pack_angle)
+            steering_angle = pack_angle - self.velocity
+
+        self.acceleration += steering_angle
+
+
+    def separation(self): # Avoid collisions
+        pass
+
+    def cohesion(self): # Boids MOVE towards the pack.
+        pass
+
+class Predator(Boid):
+    def __init__(self,x,y,WIDTH,HEIGHT):
+        Boid.__init__(self,x,y,WIDTH,HEIGHT)
+        self.position = Vector(x, y)
+        vec = (np.random.rand(2)) * 10 + 0.2
+        self.velocity = Vector(*vec)
+        vec2 = (np.random.rand(2)) / 2 + 0.2
+        self.acceleration = Vector(*vec2)
+
+        self.max_speed = 6
+        self.width = WIDTH
+        self.height = HEIGHT
+
+
+    def show(self):
+        # Triangular Bird's settings.
+        side = 14
+        tri_len = (np.sqrt(3) / 3) * side
+        tri_len2 = (np.sqrt(3) / 6) * side
+
+        fill('red')
+        A = (self.position[0], self.position[1] + tri_len)
+        B = (self.position[0] - (side / 2), self.position[1] - tri_len2)
+        C = (self.position[0] + (side / 2), self.position[1] - tri_len2)
+        triangle(A[0], A[1], B[0], B[1], C[0], C[1])
+
+
--- Final Project/BoidsProject/main.py
+++ Final Project/BoidsProject/main.py
@@ -2,6 +2,7 @@ from p5 import setup, draw, size, background, run
 import p5
 import numpy as np
 from boid import Boid
+from boid import Predator
 
 # Screen Settings.
 WIDTH = 1000
@@ -9,7 +10,10 @@ HEIGHT = 1000
 
 # Creating pack of birds at random location.
 FLOCK_SIZE = 20
+PREDATOR_SIZE = 2
+
 flock = [Boid(*np.random.rand(2)*1000, WIDTH, HEIGHT) for i in range(FLOCK_SIZE)]
+predators = [Predator(*np.random.rand(2)*1000, WIDTH, HEIGHT) for j in range(PREDATOR_SIZE)]
 
 #Initialize animation window.
 def setup():
@@ -21,6 +25,13 @@ def draw():
     for boid in flock:
         boid.boundary()
         boid.show()
+        #boid.alignment(flock)
         boid.update()
+
+    for predator in predators:
+        predator.boundary()
+        predator.show()
+        predator.update()
+
 
 run()
